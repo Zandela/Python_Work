855e2b13384a5f926e16464119e5e489c91c00c8
Zandela <changfengxiao@outlook.com>
2020-03-04 23:33:34 +0800

    列表操作

---
+++ 04/list_control.py
@@ -0,0 +1,55 @@
+# 日期：2020-03-04
+# 作者：YangZai
+# 功能：操作列表
+
+# 4-1 比萨
+pizzas = ['egg yolk shrimp pizza', 'hawaiian pizza', 'mashed potato pizza']
+for pizza in pizzas:
+    print('I like ' + pizza)
+print('I really love pizza!\n')
+
+# 4-2 动物
+animals = ['cat', 'dog', 'bird']
+for animal in animals:
+	print('A ' + animal + ' would make a great pet')
+print('Any of these animals would make a great pet!\n')
+
+# 4-3 数到20
+for value in range(1, 21):
+	print(value)
+print('\n')
+
+# 4-4 一百万
+'''
+values = []
+for value in range(1, 1000001):
+	values.append(value)
+	print(value)
+'''
+
+# 4-5 计算1～1000000的总和
+values = [value for value in range(1, 1000001)]
+print(min(values))
+print(max(values))
+print(sum(values))
+print('\n')
+# 用时0.5s
+
+# 4-6 1～20的奇数
+odds = [odd for odd in range(1, 21, 2)]
+for value in odds:
+	print(value)
+print('\n')
+
+# 4-7 3～30被3整除的数字
+numbers = [number for number in range(3, 31, 3)]
+for value in numbers:
+	print(value)
+print('\n')
+
+# 4-7 3～30被3整除的数字
+cubes = [cube ** 3 for cube in range(1, 11)]
+for value in cubes:
+	print(value)
+print('\n')
+
