2701e999ac26097a489dddd95c50d50ff4ad5d9d
Zandela <changfengxiao@outlook.com>
2020-03-10 23:55:41 +0800

    导入Restaurant类

---
+++ 09/my_restaurant.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+# 日期：2020-03-10
+# 作者：YangZai
+# 功能：导入Restaurant类
+
+# 9-10 导入Restaurant类
+import restaurant 
+
+my_restaurant = restaurant.Restaurant('老新亨手工面', '中餐馆')
+my_restaurant.describe_restaurant()
+my_restaurant.open_restaurant()
