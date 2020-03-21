1b707e0f5bd7706bd2ca2f3686f033c91388a366
Zandela <changfengxiao@outlook.com>
2020-03-21 19:18:25 +0800

    新增碰撞、记分

--- 12/alien_invasion/game_functions.py
+++ 12/alien_invasion/game_functions.py
@@ -6,6 +6,7 @@
 
 import sys
 import pygame
+from time import sleep
 
 from bullet import Bullet
 from alien import Alien
@@ -34,8 +35,30 @@ def check_keyup_events(event, ai_settings, screen, ship, bullets):
 		ship.moving_right = False
 	elif event.key == pygame.K_LEFT:
 		ship.moving_left = False
+
+def check_play_button(ai_settings, screen, stats, ship, aliens, bullets,
+	play_button, mouse_x, mouse_y):
+	'''在玩家单击Play按钮时开始新游戏'''
+	button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+	if button_clicked and not stats.game_active:
+		# 重置游戏设置
+		ai_settings.initialize_dynamic_settings()
+		# 隐藏光标
+		pygame.mouse.set_visible(False)
+
+		stats.reset_stats()
+		stats.game_active = True
+
+		# 清空外星人列表和子弹列表
+		aliens.empty()
+		bullets.empty()
+
+		# 创建一群新的外星人，并将飞船放在屏幕底端中央
+		create_fleet(ai_settings, screen, aliens)
+		ship.center_ship()
 			
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, ship, aliens, bullets,
+	play_button):
 	# 监视响应鼠标和键盘事件：
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -47,7 +70,13 @@ def check_events(ai_settings, screen, ship, bullets):
 		elif event.type == pygame.KEYUP:
 			check_keyup_events(event, ai_settings, screen, ship, bullets)
 
-def update_bullets(bullets):
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			mouse_x, mouse_y = pygame.mouse.get_pos()
+			check_play_button(ai_settings, screen, stats, ship, aliens, bullets,
+				play_button, mouse_x, mouse_y)
+			
+
+def update_bullets(ai_settings, screen, stats, aliens, bullets, score_board):
 	'''更新子弹的位置，并删除已消失的子弹'''
 	# 更新子弹位置
 	bullets.update()
@@ -56,6 +85,28 @@ def update_bullets(bullets):
 	for bullet in bullets.copy():
 		if bullet.rect.bottom <= 0:
 			bullets.remove(bullet)
+
+	# 检测碰撞并处理
+	check_bullet_alien_collisions(ai_settings, screen, stats, aliens, bullets,
+		score_board)
+
+def check_bullet_alien_collisions(ai_settings, screen, stats, aliens, bullets,
+	score_board):
+	'''响应子弹和外星人的碰撞'''
+	# 删除发生碰撞的子弹和外星人
+	collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+	if collisions:
+		for aliens in collisions.values():
+			stats.score += ai_settings.alien_points * len(aliens)
+			score_board.prep_score()
+		check_high_score(stats, score_board)
+
+	if len(aliens) == 0:
+		# 删除现有的子弹并新建一群外星人
+		bullets.empty()
+		ai_settings.increase_speed()
+		create_fleet(ai_settings, screen, aliens)
 
 def get_number_aliens_x(ai_settings, alien_width):
 	'''计算一行可以容纳多少个外星人'''
@@ -83,7 +134,8 @@ def create_fleet(ai_settings, screen, aliens):
 		# 创建一个外星人并将其加入当前行
 		create_alien(ai_settings, screen, aliens, alien_number)
 		
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets,
+	play_button, score_board):
 	'''更新屏幕上的图像，并切换到新屏幕'''
 	# 每次循环时都重新绘制屏幕
 	screen.fill(ai_settings.bg_color)
@@ -93,14 +145,28 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
 		bullet.draw()
 	ship.blitme()
 	aliens.draw(screen)
+
+	# 显示得分
+	score_board.show_score()
+
+	# 如果游戏处于非活动状态，就绘制Play按钮
+	if not stats.game_active:
+		play_button.draw_button()
 		
 	# 让最近绘制的屏幕可见
 	pygame.display.flip()
 
-def update_aliens(ai_settings, aliens):
+def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
 	'''检查是否有外星人位于屏幕边缘，并更新外星人群中所有外星人的位置'''
 	check_fleet_edges(ai_settings, aliens)
 	aliens.update()
+
+	# 检测外星人和飞船之间的碰撞
+	if pygame.sprite.spritecollideany(ship, aliens):
+		ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+
+	# 检查是否有外星人到达了屏幕底端
+	check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
 
 def check_fleet_edges(ai_settings, aliens):
 	'''有外星人到达边缘时采取相应的措施'''
@@ -114,3 +180,38 @@ def change_fleet_direction(ai_settings, aliens):
 	for alien in aliens.sprites():
 		alien.rect.y += ai_settings.fleet_drop_factor
 	ai_settings.fleet_direction *= -1
+
+def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+	'''响应外星人撞到的飞船'''
+	if stats.ships_left > 0:
+		# 将ships_left减1
+		stats.ships_left -= 1
+
+		# 清空外星人列表和子弹列表
+		aliens.empty()
+		bullets.empty()
+
+		# 创建一群新的外星人，并将飞船放在屏幕底端中央
+		create_fleet(ai_settings, screen, aliens)
+		ship.center_ship()
+
+		# 暂停
+		sleep(0.5)
+	else:
+		stats.game_active = False
+		pygame.mouse.set_visible(True)
+
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+	'''检查是否有外星人到达了屏幕底端'''
+	screen_rect = screen.get_rect()
+	for alien in aliens.sprites():
+		if alien.rect.bottom >= screen_rect.bottom:
+			# 像飞船被撞到一样进行处理
+			ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+			break
+
+def check_high_score(stats, score_board):
+	'''检查是否诞生了新的最高得分'''
+	if stats.score > stats.high_score:
+		stats.high_score = stats.score
+		score_board.prep_high_score()
--- 12/alien_invasion/settings.py
+++ 12/alien_invasion/settings.py
@@ -12,16 +12,37 @@ class Settings ():
 		self.screen_width = 1200
 		self.screen_height = 800
 		# 设置背景颜色
-		self.bg_color = (0, 0, 255)
+		self.bg_color = (230, 230, 230)
 		# 飞船设置
-		self.ship_speed_factor = 1.5	# 速度
+		self.ship_limit = 3
 		# 外星人设置
-		self.alien_speed_factor = 1		# 水平速度
-		self.fleet_drop_factor = 10		# 下落速度
-		self.fleet_direction = 1		# 1表示向右移，-1向左移
+		self.fleet_drop_factor = 100		# 下落速度
+		
 		# 子弹设置
-		self.bullet_speed_factor = 1	# 速度
 		self.bullet_width = 6
 		self.bullet_height = 30
 		self.bullet_color = (60, 60, 60)
 		self.bullets_allowed = 100
+		# 加快游戏节奏的速度
+		self.speedup_scale = 1.1
+		# 外星人点数的提高速度
+		self.score_scale = 1.5
+
+		self.initialize_dynamic_settings()
+
+	def initialize_dynamic_settings(self):
+		'''初始化随游戏进行而变化的设置'''
+		self.ship_speed_factor = 1.5
+		self.bullet_speed_factor = 3
+		self.alien_speed_factor = 1
+		self.fleet_direction = 1		# 1表示向右移，-1向左移
+		# 记分
+		self.alien_points = 50
+
+	def increase_speed(self):
+		'''提高速度设置'''
+		self.ship_speed_factor *= self.speedup_scale
+		self.bullet_speed_factor *= self.speedup_scale
+		self.alien_speed_factor *= self.speedup_scale
+
+		self.alien_points = int(self.alien_points * self.score_scale)
